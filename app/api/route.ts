d8064c0c7c2dcb1ef8ab121f35cf1b471fbdc0da
Manojnaik123 <manojnaikbse@gmail.com>
2026-03-01 11:54:08 +0530

    gr

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,8 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 import { NextResponse } from "next/server";
 
 const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    'https://yvctddhttqofusinjscs.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2Y3RkZGh0dHFvZnVzaW5qc2NzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzMTE4NDEsImV4cCI6MjA4Nzg4Nzg0MX0.fhUutDDUSFxjiZqgtFjJizrsVwwpWOS88MAaNXXNNEw'
 )
 // export default client;
 
