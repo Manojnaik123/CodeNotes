a63c0f865180d9c0068bb74d73cdf419a4abbe7e
Manojnaik123 <manojnaikbse@gmail.com>
2026-03-01 12:12:24 +0530

    gr

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,12 +1,25 @@
 import { NextResponse } from "next/server";
 import supabase from "@/lib/supabase";
+import { Folder, FolderRow } from "@/models/folder";
 
 export async function GET() {
+    const { data, error } = await supabase
+        .from("folders")
+        .select("*")
+        .returns<FolderRow[]>();
 
-    const [translationsRes] = await Promise.all([
-        supabase.from("folders").select("*"),
-    ]);
+    if (error) {
+        return NextResponse.json(
+            { error: error.message },
+            { status: 500 }
+        );
+    }
 
-    console.log(translationsRes.data);
-    return NextResponse.json(translationsRes);
+    const folders: Folder[] = data.map((row)=> ({
+        id: row.id,
+        folderName: row.folder_name,
+        createdAt: row.created_at,
+    }))
+
+    return NextResponse.json({ folders: folders });
 }
---
+++ models/folder.ts
@@ -0,0 +1,11 @@
+export type Folder = {
+    id: number;
+    folderName: string;
+    createdAt: string;
+}
+
+export type FolderRow = {
+  id: number;
+  folder_name: string;
+  created_at: string;
+};
