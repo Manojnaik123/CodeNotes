ff65f3a91056ae73ef9aa4fd33446c707d90b4ab
Manojnaik123 <manojnaikbse@gmail.com>
2026-03-01 11:58:36 +0530

    gr

--- app/api/route.ts
+++ app/api/route.ts
@@ -4,12 +4,9 @@ import supabase from "@/lib/supabase";
 export async function GET() {
 
     const [translationsRes] = await Promise.all([
-        supabase.from("BudgetTransactionCategoryTranslation").select("*"),
+        supabase.from("folders").select("*"),
     ]);
 
-
     console.log(translationsRes.data);
-
-
     return NextResponse.json(translationsRes);
 }
